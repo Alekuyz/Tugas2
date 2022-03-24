774e3744189447f9039a880c6e4094a99e9732c3
Alekuyz <axelagia@gmail.com>
2022-03-24 14:21:52 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Tugas2PABD
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            new Program().InsertData();
+        }
+        public void InsertData()
+        {
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection("data source=NITRO-AN715;Database=ExerciseSatu;User ID=sa;Password=123");
+                con.Open();
+
+                SqlCommand cm = new SqlCommand("INSERT INTO Owner (id_Owner, Nama, No_Telpon, id_Klien) VALUES ('224','Alek','0816253627','301')" +
+                    "INSERT INTO Owner (id_Owner, Nama, No_Telpon, id_Klien) VALUES ('225','Dimas','0834256748','302')" + "INSERT INTO Owner (id_Owner, Nama, No_Telpon, id_Klien) VALUES ('226','Ihsan','0845398746','303')" +
+                    "INSERT INTO Owner (id_Owner, Nama, No_Telpon, id_Klien) VALUES ('227','Paldi','0845362745','304')" + "INSERT INTO Owner (id_Owner, Nama, No_Telpon, id_Klien) VALUES ('228','Akbar','0856748392','305')", con);
+                cm.ExecuteNonQuery();
+
+                Console.WriteLine("Sukses menambahkan data");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Gagal menambahkan data, " + e);
+                Console.ReadKey();
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
